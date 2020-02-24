751d4704a1d505512056f4be493c8614d1b5e4bd
Ulisses <rasracapur@gmail.com>
2020-02-24 02:39:03 -0300

    New Achives

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Comentario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Comentario
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Testando...");
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace CursoCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+using CursoCSharp.Fundamentos;
 
-            {
-                Console.WriteLine("Olá Mundo!!!");
-            }
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentario.Executar},
+            });
+
+            central.SelecionarEExecutar();
         }
     }
 }
