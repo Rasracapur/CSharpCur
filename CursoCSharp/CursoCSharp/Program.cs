bd2b2fe101086050d288934f6ff056a2bacc637a
Ulisses <rasracapur@gmail.com>
2020-02-24 01:38:44 -0300

    Testando...

---
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            {
+                Console.WriteLine("Olá Mundo!!!");
+            }
+        }
+    }
+}
